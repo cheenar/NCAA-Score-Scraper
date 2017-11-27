eb626957c2c8f0b9c08320826972e987cd4ef1c7
Cheenar <cheenar@Berkeley.EDU>
2017-11-26 19:18:53 -0800

    Updated the scraper to handle multiple folders and added a USAGE guide

---
+++ README.md
@@ -0,0 +1,18 @@
+# NCAA Scraper
+
+## Usage
+
+In order to use the program, download a copy of the repository and then run the program.py file. The syntax for running the program is shown below.
+
+In order to have the program scrape a particular date enter the following command
+```
+#python3 program.py YYYYMMDD
+python3 program.py 20171111
+```
+
+In order to have the program scrape the current date
+```
+python3 program.py
+```
+
+The program will dump the scraped data into a Data/YYYYMMDD/ folder with each division separated by a .CSV file. 
--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-import requests, bs4, json, sys
+import requests, bs4, json, sys, os
 
 #Create the group dictionary
 #CAA=33 -> Excample
@@ -11,6 +11,25 @@ for line in group_data:
 
 user_agent = "Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>"
 accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+
+from datetime import datetime
+# Some Helper Functions for the Scraper
+def date():
+	d = str(datetime.now()).split(" ")
+	return d[0], d[1]
+
+def get_date(date):
+	return date[0]
+
+def get_time(date):
+	return date[1]
+
+def get_current_date():
+	return get_date(date()).replace("-", "")
+
+def create_folder(directory):
+	if not os.path.exists(directory):
+		os.makedirs(directory)
 
 def create_url(date, group_num):
 	base_url = "http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?lang=en&region=us&calendartype=blacklist&limit=300&dates=" + str(date) + "&tz=America%2FNew_York&groups=" + str(group_num)
@@ -32,8 +51,15 @@ def grab_data(date, group_num):
 
 def __main__():
 	for key in groups.keys():
-		data = grab_data(sys.argv[1],groups[key])
-		file = open("Data/" + key + ".csv", "+w")
+		_date = ""
+		if len(sys.argv) > 1:
+			_date = sys.argv[1]
+		else:
+			_date = get_current_date()
+		data = grab_data(_date,groups[key])
+		create_folder("Data")
+		create_folder("Data/" + _date)
+		file = open("Data/" + _date + "/" + key + ".csv", "+w")
 		file.write("Location,Name,Score\n")
 		for point in data:
 			team_a = point["team_a"]
